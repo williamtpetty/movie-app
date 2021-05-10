2de7d02f586ddaabb0f299e93de78cddb2ca132b
Petty <pettwil1131@gmail.com>
2021-05-09 19:53:02 -0500

    Adds routes for segment, query, and body params

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,13 +1,5 @@
 class ActorsController < ApplicationController
 
-  def pull_first_actor
-    render json: {actor: Actor.first}
-  end
-  
-  def pull_last_actor
-    render json: {actor: Actor.last}
-  end
-
   def pull_all_actors
     render json: {actors: Actor.all}
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/first_actor_path" => "actors#pull_first_actor"
-  get "/last_actor_path" => "actors#pull_last_actor"
-  get "/all_actors" => "actors#pull_all_actors"
+  # Gets
+  get "/all_actors" => "actors#pull_all_actors" # query
   get "/choose_actor" => "actors#choose_one_actor"
+  get "choose_single_actor/:actor_id" => "actors#choose_one_actor" # segment
 
   get "all_movies_path" => "movies#pull_all_movies"
-  get "second_movie_path" => "movies#pull_second_movie"
   get "choose_movie" => "movies#choose_one_movie"
+
+  # Posts
+  post "single_actor" => "actors#choose_one_actor" #body
+
 end
